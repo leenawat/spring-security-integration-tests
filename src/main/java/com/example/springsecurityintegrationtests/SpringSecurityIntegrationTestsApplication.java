82fa49f07761c7226991c8ca8d75954738b407f0
Leenawat Papahom <leenawat.pa@gmail.com>
2021-11-24 10:17:51 +0700

    init commit

---
+++ src/main/java/com/example/springsecurityintegrationtests/SpringSecurityIntegrationTestsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityintegrationtests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityIntegrationTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityIntegrationTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springsecurityintegrationtests/SpringSecurityIntegrationTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityintegrationtests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityIntegrationTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
